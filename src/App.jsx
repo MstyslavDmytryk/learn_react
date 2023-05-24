5a24e66ec570ecefb9a3861df8913a64c7777f10
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-05-24 12:43:13 +0300

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,32 @@ import { usePosts } from "./hooks/usePosts";
 import { useFetching } from "./hooks/useFetching";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import { getPageCount } from "./utils/pages";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  let pagesArray = [];
+  for (let i = 0; i < totalPages; i++) {
+    pagesArray.push(i + 1);
+  }
+
+  console.log(pagesArray);
+
   const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts);
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data);
+    const totalCount = response.headers["x-total-count"];
+    setTotalPages(getPageCount(totalCount, limit));
   });
+
+  console.log(totalPages);
 
   useEffect(() => {
     fetchPosts();
